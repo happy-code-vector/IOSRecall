647849ecefd192e72fa9f99091ec89fa99eb890a
happy-code-vector <kevinallen5877066@gmail.com>
2026-01-16 11:13:22 -0500

    Initial Commit

---
+++ ThinkFirst/ThinkFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkFirstApp.swift
+//  ThinkFirst
+//
+//  Created by Ahmad Rasheed on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ThinkFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
