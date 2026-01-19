a3a81952d4d50d9080bb5d89629da487174d2610
happy-code-vector <kevinallen5877066@gmail.com>
2026-01-19 10:00:06 +0300

    fix: appstate error

--- ThinkFirst/ThinkFirstApp.swift
+++ ThinkFirst/ThinkFirstApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 @main
 struct ThinkFirstApp: App {
@@ -17,61 +18,5 @@ struct ThinkFirstApp: App {
                 .environmentObject(appState)
                 .preferredColorScheme(.dark)
         }
-    }
-}
-
-// MARK: - App State
-class AppState: ObservableObject {
-    @Published var currentUser: User?
-    @Published var userProgress: UserProgress = .empty
-    @Published var hasCompletedOnboarding: Bool = false
-    @Published var showBadgeUnlock: Badge?
-    @Published var showLimitReached: Bool = false
-    @Published var showMercyModal: Bool = false
-    @Published var currentQuestion: Question?
-    @Published var currentEvaluation: Evaluation?
-    
-    init() {
-        loadUserData()
-    }
-    
-    func loadUserData() {
-        currentUser = StorageService.shared.getCurrentUser()
-        userProgress = StorageService.shared.getProgress()
-        hasCompletedOnboarding = StorageService.shared.hasCompletedOnboarding()
-    }
-    
-    func saveUser(_ user: User) {
-        currentUser = user
-        StorageService.shared.saveUser(user)
-    }
-    
-    func updateProgress(_ progress: UserProgress) {
-        userProgress = progress
-        StorageService.shared.saveProgress(progress)
-    }
-    
-    func completeOnboarding() {
-        hasCompletedOnboarding = true
-        StorageService.shared.setOnboardingCompleted(true)
-    }
-    
-    func logout() {
-        currentUser = nil
-        userProgress = .empty
-        hasCompletedOnboarding = false
-        StorageService.shared.clearUser()
-    }
-    
-    var isPremium: Bool {
-        currentUser?.isPremium ?? false
-    }
-    
-    var isStudent: Bool {
-        currentUser?.type == .student
-    }
-    
-    var isParent: Bool {
-        currentUser?.type == .parent
     }
 }
